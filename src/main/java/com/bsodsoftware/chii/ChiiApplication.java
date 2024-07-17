608c9aaba5d4929c896bbbe968322f40f494382c
Maverick Vlasilev <m4v3r1cx.thanatos@gmail.com>
2024-07-17 16:52:05 -0400

    commit inicial

---
+++ src/main/java/com/bsodsoftware/chii/ChiiApplication.java
@@ -0,0 +1,13 @@
+package com.bsodsoftware.chii;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChiiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChiiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bsodsoftware/chii/ChiiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bsodsoftware.chii;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChiiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
